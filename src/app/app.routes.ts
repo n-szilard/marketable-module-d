66d475d1f96ccbd66f127548719f04a2ca151fc6
JelencsityMiklous <jelencsitymiklos@turr.hu>
2026-02-06 11:34:15 +0100

    Service route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login';
 import { ChatComponent } from './components/chat/chat';
+import { ServicesComponent } from './components/services/services';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'chat', component: ChatComponent }
+    { path: 'services', component: ServicesComponent }
 ];
