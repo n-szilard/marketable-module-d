4ec3c437a94c52026e1843ff79a2c96e412c4874
acsnorbika <acsnorbert@turr.hu>
2026-02-06 11:27:27 +0100

    Chat componens routeolása

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login';
+import { ChatComponent } from './components/chat/chat';
 
 export const routes: Routes = [
-    { path: '', component: LoginComponent }
+    { path: '', component: LoginComponent },
+    { path: 'chat', component: ChatComponent }
 ];
