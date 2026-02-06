72124753d221f43d52d43fa364b4bd1ee81820f7
acsnorbika <acsnorbert@turr.hu>
2026-02-06 11:23:31 +0100

    Chat alapfelépítés elkezdése

--- src/app/components/chat/chat.ts
+++ src/app/components/chat/chat.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './chat.html',
   styleUrl: './chat.scss',
 })
