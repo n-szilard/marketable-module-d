9fc5fdc64a76610b050dd6af2cff36d0ac72e33d
n-szilard <nagyapatiszilard@turr.hu>
2026-02-06 11:32:26 +0100

    Bootstrap telepítés és login oldal

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-login',
-  imports: [ButtonModule],
+  imports: [CommonModule, FormsModule, ButtonModule, CheckboxModule, InputTextModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class LoginComponent {
-  
+  checked1 = signal<boolean>(true);
 }
