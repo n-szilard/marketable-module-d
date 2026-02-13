2dd084f6d7528490961373edd7e10c507950bc7a
JelencsityMiklous <jelencsitymiklos@turr.hu>
2026-02-13 10:19:31 +0100

    Services elkészítése Design + chat appba navigálás helyes token alapján

--- src/app/components/services/services.ts
+++ src/app/components/services/services.ts
@@ -1,11 +1,30 @@
+
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-services',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './services.html',
   styleUrl: './services.scss',
 })
 export class ServicesComponent {
+
+
+  constructor(private router:Router){}
+
+popUp(){
+  let token = prompt("Add meg a tokent!")
+
+  if(token=='13508a659a2dbab0a825622c43aef5b5133f85502bfdeae0b6' || token=='b8ef2feea8a2bf982d637b5ff4be4771d2ef46f3564c5ecd7b'){
+
+    this.router.navigate(['/chat'])
+
+  }
+  else{
+    alert('Hibás token!')
+  }
+
+}
 
 }
