e721d6cce5c4f62d009b9ba4d7d4a66785a040d8
n-szilard <nagyapatiszilard@turr.hu>
2026-02-20 10:13:02 +0100

    Képfelismerés feltöltés

--- src/app/components/image-recognition/image-recognition.ts
+++ src/app/components/image-recognition/image-recognition.ts
@@ -1,18 +1,57 @@
-import { Component, input } from '@angular/core';
+import { Component, input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FileUploadEvent, FileUploadModule } from 'primeng/fileupload';
+import { ButtonModule } from 'primeng/button';
+import { ApiService } from '../../services/api.service';
+
+interface UploadEvent {
+  originalEvent: Event;
+  files: File[];
+}
 
 @Component({
   selector: 'app-image-recognition',
-  imports: [],
+  imports: [FileUploadModule, ButtonModule],
   templateUrl: './image-recognition.html',
   styleUrl: './image-recognition.scss',
 })
-export class ImageRecognition {
+export class ImageRecognition implements OnInit {
 
-  constructor(private router: Router) {
-    const navigation = this.router.getCurrentNavigation();
+  private token: string | null = '';
+
+  constructor(private router: Router, private api: ApiService) {
+    const navigation = this.router.currentNavigation();
     const state = navigation?.extras.state as { token: any };
   
-    console.log(state?.token);
+    this.token = state?.token;
   }  
+
+
+  ngOnInit(): void {
+    console.log(this.token);
+  }
+
+
+  onUpload(event: FileUploadEvent) {
+
+    if (this.token == null) {
+      return;
+    }
+
+
+    const formData = new FormData();
+
+    formData.append('image', event.files[0]);
+
+    this.api.uploadFile(this.token, formData).subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
+
+
 }
